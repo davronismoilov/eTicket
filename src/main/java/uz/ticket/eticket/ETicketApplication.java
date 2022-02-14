798eb3e66f0624d9135a65373dc590a17812b2ec
davronismoilov <ismoilovdavron16@gmail.com>
2022-02-14 22:47:43 +0500

    first commit

---
+++ src/main/java/uz/ticket/eticket/ETicketApplication.java
@@ -0,0 +1,13 @@
+package uz.ticket.eticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/ticket/eticket/ETicketApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ticket.eticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
