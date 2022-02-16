1fea8f54164328ce672601b2f6ef2bdef8652bd7
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2022-02-16 08:13:37 +0500

    ticket added

---
+++ src/main/java/uz/ticket/eticket/entity/Ticket.java
@@ -0,0 +1,33 @@
+package uz.ticket.eticket.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    @ManyToOne
+    TicketType ticketType;
+
+    double price;
+    @ManyToOne
+    Location location;
+
+    int seatNumber;
+
+    long trafficId;
+
+    boolean isBusy = false;
+
+    long carriageId;
+
+}
---
+++ src/main/java/uz/ticket/eticket/enums/TicketType.java
@@ -0,0 +1,9 @@
+package uz.ticket.eticket.enums;
+
+public enum TicketType {
+    SITTING,
+    FIRST_CLASS,
+    SECOND_CLASS,
+    THIRD_CLASS,
+    LUXURY
+}
