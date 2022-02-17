fbade26ee51ad7f52797cea923e5e12f72cf3a86
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2022-02-17 15:58:59 +0500

    ticket added1111

--- src/main/java/uz/ticket/eticket/service/TicketService.java
+++ src/main/java/uz/ticket/eticket/service/TicketService.java
@@ -1,4 +1,49 @@
 package uz.ticket.eticket.service;
 
-public class TicketService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uz.ticket.eticket.entity.Ticket;
+import uz.ticket.eticket.repository.TicketRepository;
+import uz.ticket.eticket.response.ApiResponse;
+import uz.ticket.eticket.response.BaseResponse;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TicketService implements BaseResponse {
+
+    TicketRepository ticketRepository;
+
+    @Autowired
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public ApiResponse getTicketListService() {
+        List<Ticket> ticketList = ticketRepository.findAll();
+        SUCCESS.setData(ticketList);
+        return SUCCESS;
+    }
+
+    public ApiResponse addTicketService(Ticket ticket) {
+//        Ticket ticket = new Ticket();
+        Ticket save = ticketRepository.save(ticket);
+        SUCCESS.setData(ticket);
+        return SUCCESS;
+    }
+
+    public ApiResponse editTicketService(Ticket ticket) {
+        Optional<Ticket> ticketbyId = ticketRepository.findById(ticket.getId());
+        Ticket ticket1 = ticketbyId.get();
+
+        return SUCCESS;
+    }
+
+    public ApiResponse deleteTicketService() {
+        List<Ticket> ticketList = ticketRepository.findAll();
+        SUCCESS.setData(ticketList);
+        return SUCCESS;
+    }
 }
