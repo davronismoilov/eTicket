52f3f8958ea4850afa49c9a49e0f3f7b79ea5b9a
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2022-02-17 17:00:19 +0500

    ticket added1111

---
+++ src/main/java/uz/ticket/eticket/entity/Carriage.java
@@ -0,0 +1,23 @@
+package uz.ticket.eticket.entity;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Carriage {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    long id;
+    long trainId;
+    @ManyToOne
+    CarriageType carriageType;
+    @OneToMany
+    List<Seat> seatList;
+
+}
