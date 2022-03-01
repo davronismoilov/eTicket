be4da4a65a92c5814e9c40c02df512ce4c1a67e1
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2022-03-01 12:28:05 +0500

    all

--- src/main/java/uz/ticket/eticket/controller/seat/SeatController.java
+++ src/main/java/uz/ticket/eticket/controller/seat/SeatController.java
@@ -2,10 +2,8 @@ package uz.ticket.eticket.controller.seat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.ticket.eticket.entity.carriage.Carriage;
 import uz.ticket.eticket.entity.seat.Seat;
 import uz.ticket.eticket.service.seat.SeatService;
 
@@ -15,10 +13,30 @@ public class SeatController {
     @Autowired
     SeatService seatservice;
 
-    @PutMapping(value = "update")
-    public ResponseEntity<?> updateSeat(@RequestBody Seat seat) {
-        return ResponseEntity.ok(seatservice.updateSeat(seat));
+    @PutMapping(value = "update/{id}")
+    public ResponseEntity<?> updateSeat(@RequestBody Seat seat, @PathVariable long id) {
+        return ResponseEntity.ok(seatservice.updateSeat(seat, id));
     }
 
+    @GetMapping("findAll")
+    public ResponseEntity<?> getSeat() {
+        return seatservice.getSeatList();
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<?> addSeat(@RequestBody Seat carriage) {
+        return seatservice.addSeat(carriage);
+    }
+
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> deleteSeat(@PathVariable long id) {
+        return seatservice.deleteSeat(id);
+    }
+
+    @GetMapping("getById/{id}")
+    public ResponseEntity<?> getSeatById(@PathVariable long id) {
+        return seatservice.getSeatById(id);
+    }
 
 }
--- src/main/java/uz/ticket/eticket/service/seat/SeatService.java
+++ src/main/java/uz/ticket/eticket/service/seat/SeatService.java
@@ -1,10 +1,15 @@
 package uz.ticket.eticket.service.seat;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uz.ticket.eticket.entity.seat.Seat;
 import uz.ticket.eticket.repository.seat.SeatRepository;
 import uz.ticket.eticket.response.ApiResponse;
 import uz.ticket.eticket.response.BaseResponse;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -18,12 +23,50 @@ public class SeatService {
     }
 
 
-    public ApiResponse updateSeat(Seat seat) {
-        Seat save = seatRepository.save(seat);
+    public ResponseEntity<?> updateSeat(Seat seat, long id) {
+        Optional<Seat> byId = seatRepository.findById(id);
+        if (byId.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse.getUSER_NOT_FOUND());
+        seat.setId(id);
         ApiResponse SUCCESS = baseResponse.getSUCCESS();
-        SUCCESS.setData(save);
-        return SUCCESS;
+        SUCCESS.setData(seat);
+        return ResponseEntity.ok(SUCCESS);
     }
 
 
+    public ResponseEntity<?> getSeatList() {
+        List<Seat> all = seatRepository.findAll();
+        if (all.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse.getUSER_NOT_FOUND());
+        ApiResponse SUCCESS = baseResponse.getSUCCESS();
+        SUCCESS.setData(all);
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    public ResponseEntity<?> addSeat(Seat seat) {
+        Seat save = seatRepository.save(seat);
+        ApiResponse success = baseResponse.getSUCCESS();
+        success.setData(save);
+        return ResponseEntity.ok(success);
+    }
+
+    public ResponseEntity<?> deleteSeat(long id) {
+        Optional<Seat> byId = seatRepository.findById(id);
+        if (byId.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse.getUSER_NOT_FOUND());
+        Seat seat = byId.get();
+        seat.setActive(false);
+        ApiResponse success = baseResponse.getSUCCESS();
+        success.setData(seat);
+        return ResponseEntity.ok(success);
+    }
+
+    public ResponseEntity<?> getSeatById(long id) {
+        Optional<Seat> byId = seatRepository.findById(id);
+        if (byId.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse.getUSER_NOT_FOUND());
+        ApiResponse success = baseResponse.getSUCCESS();
+        success.setData(byId.get());
+        return ResponseEntity.ok(success);
+    }
 }
